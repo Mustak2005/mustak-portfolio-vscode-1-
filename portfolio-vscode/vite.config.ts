966c873a0cc09022eb11d973ecc5bef82d4aa01d
Mustak2005 <24691A05X9@mits.ac.in>
2026-08-16 17:46:05 +0530

    Set base path for Vite configuration

--- portfolio-vscode/vite.config.ts
+++ portfolio-vscode/vite.config.ts
@@ -4,16 +4,21 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
+  base: "/mustak-portfolio-vscode-1/",
+
   plugins: [react(), tailwindcss()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
       "@assets": path.resolve(__dirname, "src/assets"),
     },
   },
+
   build: {
     outDir: "dist",
   },
+
   server: {
     port: 5173,
   },
