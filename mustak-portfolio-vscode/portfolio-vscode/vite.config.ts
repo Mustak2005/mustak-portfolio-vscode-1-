0c288e02b89ea680031ee8a327b2d878ca04b066
Mustak2005 <24691A05X9@mits.ac.in>
2026-08-19 19:23:01 +0530

    Update Vite config for path resolution

--- mustak-portfolio-vscode/portfolio-vscode/vite.config.ts
+++ mustak-portfolio-vscode/portfolio-vscode/vite.config.ts
@@ -4,16 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
+  base: "/mustak-portfolio-vscode-1/",
+
   plugins: [react(), tailwindcss()],
 
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@": path.resolve(__dirname, "src"),
+      "@assets": path.resolve(__dirname, "src/assets"),
     },
   },
-
-  base: "/mustak-portfolio-vscode-1/",
 
   build: {
     outDir: "dist",
